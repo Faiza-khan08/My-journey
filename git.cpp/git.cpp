3befd9622c0273f034be68f5ddb97707294f53dd
Faiza-khan08 <faizaazam003@gmail.com>
2026-04-14 02:05:44 +0800

    first  commit

---
+++ git.cpp/git.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "My first GitHub project!" << endl;
+    return 0;
+}
+
---
+++ git.cpp/my journey/git.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "My first GitHub project!" << endl;
+    return 0;
+}
